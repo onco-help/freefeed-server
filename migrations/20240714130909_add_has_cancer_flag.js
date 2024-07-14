37ce068b01db10b2d18ceb861ca68fa6bf63675a
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2024-07-14 20:57:57 +0100

    fix migration

--- migrations/20240714130909_add_has_cancer_flag.js
+++ migrations/20240714130909_add_has_cancer_flag.js
@@ -3,11 +3,15 @@ import type { Knex } from 'knex';
 export const up = function (knex: Knex) {
   knex.schema.raw(`do $$begin
 
+  alter table users add column has_cancer boolean not null default false;
+
   end$$`);
 };
 
 export const down = function (knex: Knex) {
   knex.schema.raw(`do $$begin
+
+  alter table users drop column has_cancer;
 
   end$$`);
 };
